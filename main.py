a29dbf548c5d46623202484185e62243e73723e1
Bruno ROCHA PEREIRA <brochape@ulb.ac.be>
2014-10-06 15:37:06 +0200

    Now handles desktop notifications

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import fbconsole
+#Dependencies: libnotify, python-gobject (or python2-gobject for Python 2) 
 
-fbconsole.ACCESS_TOKEN = "sdhgqsfguypiùmsbqdhv"
+import fbconsole
+import subprocess
+import os
+
+fbconsole.ACCESS_TOKEN = "aaa"#Put your token here
+
+f = open('notifications.txt', 'w')
+path =  os.path.dirname(os.path.realpath(__file__))
 
 for notif in fbconsole.iter_pages(fbconsole.get("/me/notifications")):
-    print notif
+    subprocess.call(["notify-send","From : " + notif[u'from'][u'name'],notif[u'title'] + "\n" + notif[u'link'], "--icon=" + path +"/index.jpg" ])
