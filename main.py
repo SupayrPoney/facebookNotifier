e6b1f024364c2bde59d2705323363dae3b6589b4
Bruno ROCHA PEREIRA <brochape@ulb.ac.be>
2014-10-07 16:02:54 +0200

    Now automatically gets the token and writes the pid in a file so it can be killed easily

--- main.py
+++ main.py
@@ -3,11 +3,24 @@
 import fbconsole
 import subprocess
 import os
+import time
 
-fbconsole.ACCESS_TOKEN = "aaa"#Put your token here
 
-f = open('notifications.txt', 'w')
-path =  os.path.dirname(os.path.realpath(__file__))
+fbconsole.AUTH_SCOPE = ['manage_notifications']
+fbconsole.authenticate()
 
-for notif in fbconsole.iter_pages(fbconsole.get("/me/notifications")):
-    subprocess.call(["notify-send","From : " + notif[u'from'][u'name'],notif[u'title'] + "\n" + notif[u'link'], "--icon=" + path +"/index.jpg" ])
+pidFile = open("pidFile.txt",'w')#Keeps the pid so it can be killed easily
+pidFile.write(str(os.getpid()))
+pidFile.close()
+
+recievedNotifications = []
+while(True):
+    path =  os.path.dirname(os.path.realpath(__file__))
+
+    for notif in fbconsole.iter_pages(fbconsole.get("/me/notifications")):
+        print notif
+        if (notif[u'id'] not in recievedNotifications):
+            subprocess.call(["notify-send","From : " + notif[u'from'][u'name'],notif[u'title'] + "\n" + notif[u'link'], "--icon=" + path +"/index.jpg" ])
+            recievedNotifications.append(notif[u'id'])
+
+    time.sleep(30)
